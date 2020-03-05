6aaf21ad2a4ef8c6ea296c2db7408a30560de08a
Vitaly Kazin <vetalka@users.noreply.github.com>
2020-03-05 10:58:46 +0200

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld{
   public static void main(String[]args){
-    System.out.println("____****Hello World & webhook vitaly****____");
+    System.out.println("____****Hello World & webhook itai****____");
   }
 }
 
