a2a65115a125953fd9c624924998da3b3f9d304f
Vitaly Kazin <vetalka@users.noreply.github.com>
2020-03-05 10:00:44 +0200

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld{
   public static void main(String[]args){
-    System.out.println("____****Hello World & welcome to Jenkins Training test vitaly****____");
+    System.out.println("____****Hello World & welcome to Jenkins Training test vitaly kazin****____");
   }
 }
 
